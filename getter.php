105a7de2cd486f45134c8676c8a086e8630689d3
SeaBee <85791528+seabee33@users.noreply.github.com>
2024-09-07 19:25:26 +1000

    Update getter.php

--- getter.php
+++ getter.php
@@ -12,31 +12,62 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_GET["q"] == "binance-spot"){
-	$result = $conn->query("SELECT ticker FROM tickers WHERE exchange='binance' AND category='spot'");
-} elseif($_GET["q"] == "binance-futures"){
-	$result = $conn->query("SELECT ticker FROM tickers WHERE exchange='binance' AND category='usdsm'");
-} elseif($_GET["q"] == "bybit-futures"){
-	$result = $conn->query("SELECT ticker FROM tickers WHERE exchange='bybit' AND category='futures'");
-} else {
-	die("Dam, something broke :(");
-}
+if(isset($_GET["q"])){
+	$isFutures = False;
+    switch($_GET["q"]){
 
-$tickers = [];
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-		$ticker = $row["ticker"];
-        if ($_GET['q'] == "binance-spot"){
-			$tickers[] = "BINANCE:{$ticker}";
-		} elseif($_GET['q'] == "binance-futures"){
-			$tickers[] = "BINANCE:{$ticker}.P";
-		} elseif($_GET['q'] == "bybit-futures"){
-			$tickers[] = "BYBIT:{$ticker}.P";
+        // BINANCE
+        case "binance-spot":
+            $exchange = "binance";
+            $exchangeResult = "BINANCE";
+            $category = "spot";
+            break;
+        case "binance-futures":
+            $exchange = "binance";
+            $exchangeResult = "BINANCE";
+            $category = "usdsm";
+	    $isFutures = True;
+            break;
+        
+        // BYBIT
+        case "bybit-futures":
+            $exchange = "bybit";
+            $exchangeResult = "BYBIT";
+            $category = "futures";
+			$isFutures = True;
+            break;
+
+        // MEXC
+        case "mexc-futures":
+            $exchange = "mexc";
+            $exchangeResult = "MEXC";
+            $category = "futures";
+			$isFutures = True;
+            break;
+        case "mexc-spot":
+            $exchange = "mexc";
+            $exchangeResult = "MEXC";
+            $category = "spot";
+			$isFutures = True;
+            break;
+        default:
+            die("Something broke");
+    }
+    $result = $conn->query("SELECT ticker FROM tickers WHERE exchange='$exchange' AND category='$category'");
+
+    if(!$result){
+        die("query failed, error: " . $conn->error);
+    }
+
+    $tickers = [];
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $ticker = $row["ticker"];
+            $tickers[] = $exchangeResult . ":" . $ticker . ($isFutures ? ".P" : "");
 		}
     }
+    echo implode(",", $tickers);
+    $conn->close();
 }
 
-echo implode(",", $tickers);
-
-$conn->close();
 ?>
